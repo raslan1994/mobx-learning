6cd17dd984581ec4799be9a2590c9d260acb202f
Raslan Rauff <raslan@thesoftwarepractice.com>
2019-01-25 09:42:32 +0530

    minor alignment

--- App.js
+++ App.js
@@ -9,18 +9,29 @@
 
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button} from 'react-native';
+// import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
 export default class App extends Component{
+  state = {
+    count: 2
+  }
   render() {
-    let count = 1;
-
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>First Mob-x</Text>
         <View style={styles.instructions}>
-          <Text>Count: {count}</Text>
-          <Button title="Increase"/>
-          <Button title="Decrease"/>
+          <Text>Count: {this.state.count}</Text>
+          <Button 
+            title="+ Increase"
+            onPress={()=>{
+              this.setState({ count: this.state.count + 1 });
+            }}/>
+          <Button 
+            title="- Decrease"
+            onPress={()=>{
+              this.setState({ count: this.state.count - 1 });
+            }}/>
         </View>
       </View>
     );
